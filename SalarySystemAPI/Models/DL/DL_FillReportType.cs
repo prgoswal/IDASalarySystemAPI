fee30c3b544bad28bad3bc1175102956d13d0dd5
prgoswal <39828677+prgoswal@users.noreply.github.com>
2018-06-05 18:41:26 +0530

    Change in the Data Access layer.
    Change in the Data Access layer.

--- SalarySystemAPI/Models/DL/DL_FillReportType.cs
+++ SalarySystemAPI/Models/DL/DL_FillReportType.cs
@@ -38,6 +38,29 @@ namespace SalarySystemAPI.Models.DL
                 con.Dispose();
             }
         }
+        
+         internal DataSet Dl_FillSelectTypes(int Ind = 0, int CityCode = 0)
+        {
+            try
+            {
+                con = Connection.SetConnection();
+                cmd = new SqlCommand("SpFillReportType", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Ind", Ind);
+                cmd.Parameters.AddWithValue("@CityCode", CityCode);
+                da = new SqlDataAdapter(cmd);
+                ds = new DataSet();
+                da.Fill(ds);
+                return ds;
+
+            }
+            catch { return ds; }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+        }
     }
 
 
